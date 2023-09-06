20147d61562857ab586dbd5ecc78a548fed8df06
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-09-06 09:32:40 +0800

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-tag=`curl https://api.github.com/repos/lbl8603/vnt/releases/latest | grep 'tag_name' | cut -d\" -f4`
+tag=`curl https://api.github.com/repos/alist-org/alist/releases/latest | grep 'tag_name' | cut -d\" -f4`
 echo tag=$tag
 cd /opt/st
-curl -L https://github.com/alist-org/alist/releases/download/$tag/alist-linux-musl-mipsle.tar.gz -O alist-linux-musl-mipsle.tar.gz
+curl -fkL https://github.com/alist-org/alist/releases/download/$tag/alist-linux-musl-mipsle.tar.gz -O alist-linux-musl-mipsle.tar.gz
 tar -xzvf alist-linux-musl-mipsle.tar.gz -C /opt/st
