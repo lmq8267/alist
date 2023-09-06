83e45414c83e1341e565991e254e5dd99ea70842
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-09-06 10:27:46 +0800

    Delete up.sh

--- up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-tag=`curl https://api.github.com/repos/alist-org/alist/releases/latest | grep 'tag_name' | cut -d\" -f4`
-echo tag=$tag
-cd /opt/st
-curl -k -L -o /opt/ali/alist-linux-musl-mipsle.tar.gz "https://github.com/alist-org/alist/releases/download/${tag}/alist-linux-musl-mipsle.tar.gz"
-tar -xzvf /opt/ali/alist-linux-musl-mipsle.tar.gz -C /opt/st
